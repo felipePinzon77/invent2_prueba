da2e4645cb93d7d717c3b780a8f5db0215841ac8
ckack2226 <113803197+ckack2226@users.noreply.github.com>
2024-03-27 16:41:58 -0500

    se creo componente sign up

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 export const routes: Routes = [
     {path:'', redirectTo:'login', pathMatch:'full'},
     {path:'login',component:LoginComponent},
-    {path:'', 
-    component:InicioComponent
-}
+    {path:"signUp", component:SignUpComponent},
+    {path:'inicio', component:InicioComponent}
 ];
